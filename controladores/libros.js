20c2c4fffcae94a3d639ba0952983689e733ec48
jaredsolanosand <158521596+jaredsolanosand@users.noreply.github.com>
2024-02-25 18:49:07 -0600

    Update libros.js

--- controladores/libros.js
+++ controladores/libros.js
@@ -1,21 +1,19 @@
-const modeloLibro = require('../modelos/libros.js')
-var libros = {}
+const modeloLibro = require('../modelos/libros.js');
+const { v4: uuidv4 } = require('uuid');
+var libros = {};
+
 module.exports = {
 
     crearLibros: (peticion, respuesta) => {
-        if (libros[peticion.params.id]) {
-            respuesta.status(409).json({
-                name: 'ID de libro ya existe',
-                message: `Ya existe un libro con el ID ${peticion.params.id}`
-            })
-            return
-        }
-    
-        libros[peticion.params.id] = modeloLibro(
-            peticion.params.id, peticion.params.titulo, peticion.params.autores, peticion.params.editorial, peticion.params.fechaPublicacion
-        )
-        let urlNuevoLibro = `/Libros/${peticion.params.id}`
-        respuesta.status(201).send(urlNuevoLibro)
+        const id = uuidv4(); 
+        const nuevoLibro = modeloLibro(
+            id, peticion.params.titulo, peticion.params.autores, peticion.params.editorial, peticion.params.fechaPublicacion
+        );
+
+        libros[id] = nuevoLibro;
+
+        let urlNuevoLibro = `/libros/${id}`;
+        respuesta.status(201).send(urlNuevoLibro);
     },
 
     borrarLibro: (peticion, respuesta) => {
@@ -23,7 +21,6 @@ module.exports = {
         let urlLibroBorrado = `/libros/${peticion.params.id}`
         respuesta.send(urlLibroBorrado)
     },
-
     obtenerLibro: (peticion, respuesta) => {
         if (!libros[peticion.params.id]) {
             respuesta.status(404).json({
@@ -35,15 +32,79 @@ module.exports = {
         respuesta.send(lbr.toString())
     },
 
+    buscarLibros: (titulo, autores, fechaInicio, fechaFin) => {
+
+        const tituloMinusculas = titulo ? titulo.toLowerCase() : '';
+        const autoresMinusculas = autores ? autores.toLowerCase() : '';
+
+        const librosEncontrados = Object.values(libros).filter(libro => {
+
+            const tituloLibro = libro.obtenerTitulo().toLowerCase();
+            const autoresLibro = libro.obtenerAutores().toLowerCase();
+            
+            const tituloCoincide = !titulo || tituloLibro.includes(tituloMinusculas);
+        
+            const autoresCoinciden = !autores || autoresLibro.includes(autoresMinusculas);
+            
+            let fechaPublicacion = libro.obtenerFechaPublicacion();
+            const fechaInicioValida = !fechaInicio || fechaPublicacion >= fechaInicio;
+            const fechaFinValida = !fechaFin || fechaPublicacion <= fechaFin;
+            
+            return tituloCoincide && autoresCoinciden && fechaInicioValida && fechaFinValida;
+        }).map(libro => {
+            return {
+                id: libro.obtenerId(),
+                titulo: libro.obtenerTitulo(),
+                autores: libro.obtenerAutores(),
+                editorial: libro.obtenerEditorial(),
+                fechaPublicacion: libro.obtenerFechaPublicacion()
+            };
+        });
+    
+        console.log("Libros encontrados:", librosEncontrados);
+    
+        return librosEncontrados;
+    },
+     
+    obtenerDetallesDelLibro: (id) => {
+        const libro = libros[id];
+        if (libro) {
+            return {
+                id: libro.obtenerId(),
+                titulo: libro.obtenerTitulo(),
+                autores: libro.obtenerAutores(),
+                editorial: libro.obtenerEditorial(),
+                fechaPublicacion: libro.obtenerFechaPublicacion()
+            };
+        } else {
+            return null; 
+        }
+    },
+    
+    mostrarFormularioModificarLibro: (peticion, respuesta) => {
+        const id = peticion.params.id;
+        console.log("ID QUE SE VA A MODIFICAR:", id);
+        const libro = libros[id]; 
+        if (!libro) {
+            return respuesta.status(404).json({
+                message: `No se encontró un libro con el ID ${id}`
+            });
+        }
+        const vistasAnteriores = ['Pantalla Principal', 'Libros Buscados', 'Detalles Libro'];
+        const nombreVistaActual = 'Editar Libro';
+    
+        respuesta.render('ModificarLibro', { id, libro, vistasAnteriores, nombreVistaActual });
+    },
+    
     modificarLibro: (peticion, respuesta) => {
         const id = peticion.params.id;
-
+        console.log("ID QUE SE VA A MODIFICAR JEJEJE:", id);
         if (!libros[id]) {
             return respuesta.status(404).json({
                 message: `No se encontró un libro con el ID ${id}`
             });
         }
-
+    
         libros[id] = modeloLibro(
             id, 
             peticion.params.titulo, 
@@ -51,10 +112,14 @@ module.exports = {
             peticion.params.editorial, 
             peticion.params.fechaPublicacion
         );
-
+    
+        // Respuesta JSON para confirmar la modificación exitosa
         respuesta.json({
             message: `Libro con ID ${id} modificado exitosamente`
         });
     }
-
+    
+    
+    
 }
+
